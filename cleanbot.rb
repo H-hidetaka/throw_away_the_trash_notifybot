e8da00c89c3a9e8befe17a604e2f59849d59664f
H-hidetaka <sansanhidetaka@gmail.com>
2022-07-20 01:10:53 +0900

    [Add]aiueo

--- cleanbot.rb
+++ cleanbot.rb
@@ -2,65 +2,55 @@ require 'line_notify'
 require 'pry'
 require 'date'
 
-def Clean_days_of_the_week
-  residents = ["有馬", "須藤", "長澤", "相澤", "荒牧", "市塙", "宮原"]
-
-  residents.each do |person|
-    # 日付の取得
-    time = Time.now
-    days_of_the_week = Date.today.wday
-    days = ["日曜日","月曜日","火曜日","水曜日","木曜日","金曜日","土曜日"]
-    ans = "今日は#{days[days_of_the_week]}です"
-    # day = time.day
-    # person = "#{day}日#{days_of_the_week}日のゴミ出し当番は#{person[residents]}です。"
-    # puts "#{day}日目〜#{week}日目のゴミ出し当番は#{person}です。"
-    return ans
-    # return person
-  end
-
+def time_of_today
+  time = Time.now
+  year = time.year
+  month = time.month
+  day = time.day
+  hour = time.hour
+  min = time.min
+  today = "今日は" + year.to_s + "年" + month.to_s + "月" + day.to_s + "日"
 end
 
-def Garbage_duty
-  residents = ["有馬", "須藤", "長澤", "相澤", "荒牧", "市塙", "宮原"]
-  residents.each do |person|
-    days_of_the_week = Date.today.wday
-    day = time.day
-    on_duty = "#{day}日#{days_of_the_week}日のゴミ出し当番は#{person[residents]}です。"
-    return on_duty
-  end
+def days_of_the_week
+  days_of_the_week = Date.today.wday
+  days = ["日曜日","月曜日","火曜日","水曜日","木曜日","金曜日","土曜日"]
+  day = "#{days[days_of_the_week]}です。"
 end
 
-  # 日付を取得
-  # 日付に照らし合わせて、曜日を選択
-  # 曜日によって出力される言葉が変わる。
+def residents_of_Garbage_duty
+  # 住人
+  residents = ["有馬", "長澤", "市塙", "相澤", "荒牧", "宮原", "久保田"]
+  # 基準日
+  # base_day = Time.new(2022,5,23)
+  # 曜日
+  # days_of_the_week = Date.today.wday
+  # days = ["日曜日","月曜日","火曜日","水曜日","木曜日","金曜日","土曜日"]
+  # day = "#{days[days_of_the_week]}"
+  residents
+end
+
+days_of_the_week = Date.today
+
+base_day = Time.new(2022,5,16)
+
+# a = 365 / 6 / 7
+# p a
+# puts time_of_today
+# puts days_of_the_week
 
 line_notify = LineNotify.new("03mmMtw2nuoxi1LLGIX06Jqudgz2EEcvpVvuKPjuHqX")
-# line_notify2 = LineNotify.new("cdRU6MrgM2bzzYN03VToQr3W2fyXqWI8VUyvIsMseoH") # LINE Notifyのアクセストークン(適宜変更)
-# line_notify = LineNotify.new("aqndBAQoi9PI2kZB2dZhAuiaVvj7jK2ysWiVX8aVGnf") # LINE Notifyのアクセストークン(適宜変更)
-# １、ゴミを出す人を順番に決める。
-# ２、ゴミを出す人が１週間経ったら、違う人に切り替わるようにする。
-
-# １週間
-# 0..6
-# 月（０）〜日（６）
-
-# 一日
-# 0
-
-# binding.pry
-# 2-1,
-# if residents ==
-#   puts ゴミ捨て当番です！
-# else residents = "須藤"
-# else residents = "長澤"
-# else residents = "相澤"
-# else residents = "長澤"
-# else residents = "市塙"
-# else residents = "宮原"
-
-options = {message: "#{Clean_days_of_the_week}、今週の当番は#{Garbage_duty}です",
-  notificationDisabled: true}
-# binding.pry
+options = {message: "#{time_of_today}です。今週の当番は#{residents_of_Garbage_duty[every_day]}です。",
+# imageFullsize: "https://www.pakutaso.com/shared/img/thumb/SAYAPAKU5347_TP_V4.jpg",
+# imageThumbnail: "https://www.pakutaso.com/shared/img/thumb/SAYAPAKU5347_TP_V.jpg",
+notificationDisabled: true
+# stickerPackageId: 1, stickerId: 113
+}
 line_notify.ping(options)
-# line_notify2.ping(options)
+
 puts "LINE botの動作が正常に終了しました。"
+
+p time_of_today
+# p days_of_the_week
+puts days_of_the_week + 7
+# p base_day
--- ruby_bot.rb
+++ ruby_bot.rb
@@ -1,5 +1,6 @@
 require 'line_notify'
 require 'pry'
+require 'date'
 
 # line_notify = LineNotify.new("cdRU6MrgM2bzzYN03VToQr3W2fyXqWI8VUyvIsMseoH") # LINE Notifyのアクセストークン(適宜変更)
 line_notify = LineNotify.new("03mmMtw2nuoxi1LLGIX06Jqudgz2EEcvpVvuKPjuHqX") # LINE Notifyのアクセストークン(適宜変更)
@@ -9,7 +10,7 @@ line_notify = LineNotify.new("03mmMtw2nuoxi1LLGIX06Jqudgz2EEcvpVvuKPjuHqX") # LI
 # ３、変数をStickerIDに入れる。
 # ４、動作確認
 @random = rand(52114110..52114149)
-options = {message: 'ﾀｽｹﾃ・・・ﾀｽｹﾃ', stickerPackageId: 11539, stickerId: @random, notificationDisabled: true}
+options = {message: "", stickerPackageId: 11539, stickerId: @random, notificationDisabled: true}
 # binding.pry
 line_notify.ping(options)
 puts "LINE botの動作が正常に終了しました。"
